ad7607075dd6f972123695cf75c0a7227fd5ce49
Joshua Burleson <joshua.burleson.developer@gmail.com>
2021-06-09 14:14:13 -0400

    improve private attribute and method naming to convention

--- AccessPanel/AccessPanel.h
+++ AccessPanel/AccessPanel.h
@@ -14,80 +14,81 @@ class AccessPanel {
     public:
         byte cols = 4;
         byte rows = 4;
+
         AccessPanel( byte rowsPins[4], byte columnsPins[4] ) {
             for( int i = 0; i < 4; i++ ) {
-                this->accessCode[i] = EEPROM.read(i);
-                this->rowPins[i] = rowsPins[i];
-                this->colPins[i] = columnsPins[i];
+                this->_accessCode[i] = EEPROM.read(i);
+                this->_rowPins[i] = rowsPins[i];
+                this->_colPins[i] = columnsPins[i];
             }
         };
 
-        Keypad keypad = Keypad(makeKeymap(this->fourFourKeys), this->rowPins, this->colPins, this->rows, this->cols);
+        Keypad keypad = Keypad(makeKeymap(this->_fourFourKeys), this->_rowPins, this->_colPins, this->rows, this->cols);
         
         void setAccessCode( char newAccessCode[4] ) {
           for( int i = 0; i < 4; i++ ) {
-            this->accessCode[i] == newAccessCode[i];
+            this->_accessCode[i] == newAccessCode[i];
             EEPROM.write(i, newAccessCode[i]);
           }
-        }
+        };
 
         void scanForInput() {
           char key = this->keypad.getKey();
           if( key ) this->processKeyPress( key );
-         }
+         };
 
          bool requestAccess() {
-          return this->accessGranted;
-         }
+          return this->_accessGranted;
+         };
 
          void resetAccess() {
-          this->accessGranted = false;
-          this->resetCurrentCode();
-         }
+          this->_accessGranted = false;
+          this->_resetCurrentCode();
+         };
          
     private:
-        bool accessGranted = false;
-        int currentCodePosition = 0;
-        char accessCode[4] = {'0', '0', '0', '0'};
-        char currentCode[4] = {'x', 'x', 'x', 'x'};
-        char fourFourKeys[4][4] = {
+        bool _accessGranted = false;
+        int _currentCodePosition = 0;
+        char _accessCode[4] = {'0', '0', '0', '0'};
+        char _currentCode[4] = {'x', 'x', 'x', 'x'};
+        char _fourFourKeys[4][4] = {
           {'1', '2', '3', 'A'},
           {'4', '5', '6', 'B'},
           {'7', '8', '9', 'C'},
           {'*', '0', '#', 'D'}
         };
-        byte colPins[4] = {6, 7, 8, 9};
-        byte rowPins[4] = {2, 3, 4, 5};
+        byte _colPins[4] = {6, 7, 8, 9};
+        byte _rowPins[4] = {2, 3, 4, 5};
         
         void processKeyPress( char key ) {
-           if( key == '#' )  this->accessGranted = this->checkCode();
+           if( key == '#' )  this->_accessGranted = this->_checkCode();
            else {
-            if ( this->currentCodePosition == 4 ) {
-              this->resetCurrentCode();
-              this->currentCode[0] = key;
-              this->currentCodePosition++;
+            if ( this->_currentCodePosition == 4 ) {
+              this->_resetCurrentCode();
+              this->_currentCode[0] = key;
+              this->_currentCodePosition++;
               return;
             }
-            this->currentCode[ this->currentCodePosition ] = key;
-            this->currentCodePosition++;
+            this->_currentCode[ this->_currentCodePosition ] = key;
+            this->_currentCodePosition++;
            }
-         }
+         };
 
-         bool checkCode() {
+         bool _checkCode() {
             for( int i = 0; i < 4; i++ ) {
-              if( !( this->accessCode[i] == this->currentCode[i] ) ) {
-                this->resetCurrentCode();
+              if( !( this->_accessCode[i] == this->_currentCode[i] ) ) {
+                this->_resetCurrentCode();
                 return false;
               }
             }
-            this->resetCurrentCode();
+            this->_resetCurrentCode();
             return true;
-         }
+         };
          
-         void resetCurrentCode() {
-          for( int i = 0; i < 4; i++ ) currentCode[i] = 'x'; 
-          this->currentCodePosition = 0;
-         }
+         void _resetCurrentCode() {
+          for( int i = 0; i < 4; i++ ) this->_currentCode[i] = 'x'; 
+          this->_currentCodePosition = 0;
+         };
 
 };
 
