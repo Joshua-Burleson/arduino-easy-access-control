f76e24b4b67471fd4a6bfe15c1ccd68dff2b6fa0
Joshua Burleson <joshua.burleson.developer@gmail.com>
2021-06-22 17:49:48 -0400

    adds motor start and stop methods to reduce 'chitter' from servo

--- LockServo/LockServo.cpp
+++ LockServo/LockServo.cpp
@@ -10,6 +10,21 @@ LockServo::LockServo( bool startLocked = true ) {
   this->setLockedState( startLocked );
 };
 
+LockServo::LockServo( int pinNumber ) {
+  this->_pin = pinNumber;
+  this->setLockedState( true );
+};
+
+LockServo::LockServo( int pinNumber, bool startLocked = true ) {
+  this->_pin = pinNumber;
+  this->setLockedState( startLocked );
+};
+
+void LockServo::start() {
+  this->setPin( this->_pin );
+  this->setLockedState( this->_lockedState );
+}
+
 void LockServo::calibrationCycle( int first = 0, int second = 45, int third = 90, int fourth = 135, int fifth = 180, bool finalPositionLocked = true ) {
   delay(4500);
   this->write(first);
@@ -33,6 +48,10 @@ void LockServo::lock() {
   this->write( this->_locked );
   this->_lockedState = true;
 };
+void LockServo::setPin( int pinNumber ) {
+  this->_pin = pinNumber;
+  this->attach( pinNumber );
+};
 void LockServo::setLockedPosition( int lockedAngle ){
     this->_locked = lockedAngle;
 };
@@ -41,6 +60,13 @@ void LockServo::setUnlockedPosition( int unlockedAngle ){
 };
 void LockServo::setLockedState( bool newLockedState ) {
   newLockedState ? this->lock() : this->unlock();
+};
+void LockServo::stopMotor() {
+  delay(10);
+  this->detach();
+};
+void LockServo::startMotor() {
+  this->attach( this->_pin );
 };
 bool LockServo::isLocked() {
   return this->_lockedState;
--- LockServo/LockServo.h
+++ LockServo/LockServo.h
@@ -12,17 +12,24 @@
 class LockServo: public Servo {
   public:
     LockServo(bool startLocked);
+    LockServo(int pinNumber);
+    LockServo(int pinNumber, bool startLocked);
     void calibrationCycle( int first, int second, int third, int fourth, int fifth, bool finalPositionLocked );
     void unlock();
     void lock();
+    void setPin( int pinNumber );
     void setLockedPosition( int lockedAngle );
     void setUnlockedPosition( int unlockedAngle );
     void setLockedState( bool newLockedState );
+    void start();
+    void startMotor();
+    void stopMotor();
     bool isLocked();
   private:
     int _angle = 86;
     int _unlocked = 170;
     int _locked = 86;
+    int _pin = 10;
     bool _lockedState = true;
 };
 
